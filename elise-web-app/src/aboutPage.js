38dfc3925bca9d2a414cdd0e0f8d676139459bc6
EliseSantella <elise.santella@gmail.com>
2025-03-31 19:51:37 +1100

    Add photo & text to about

--- elise-web-app/src/aboutPage.js
+++ elise-web-app/src/aboutPage.js
@@ -2,14 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './StyleSheets/aboutPage.css'; 
 
-const Home = () => {
-    return (
-      <div>
-               <h1 className="titleText">Hi, I'm Elise Santella</h1>
-           
+const About = () => {
+    return (  
+      <div className="backgroundWrapper">
+        <div className="background"> {/* Background image here */}
+          <div className="aboutContainer">
+            <div className="photoContainer">
+              <div className="buttonMoreContainer">
+                <button className="findMoreButton">
+                  <h5 className="buttonMoreText">FIND OUT MORE</h5> 
+                </button>
+              </div>
+            </div>
+            <div className="aboutMeContainer">
+              <div className="aboutTitleContainer">
+                <h1 className="aboutTitleText">It's all about me!</h1>
+                {/* Wrap the button inside a div with Link */}
+                <Link to="/">
+                  <div className="homeIconWrapper">
+                    <button className="homeIconContainer"></button>
+                  </div>
+                </Link>
+              </div>
+              <h5 className="aboutBodyText">INSERT TEXT SOMEHOW</h5>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 
-export default Home;
+export default About;
 
