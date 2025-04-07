e99f477907827475c63414e72f61564de3e3d568
EliseSantella <elise.santella@gmail.com>
2025-04-07 18:00:48 +1000

    Add cake image to page

--- elise-web-app/src/portfolioPage.js
+++ elise-web-app/src/portfolioPage.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './StyleSheets/portfolioPage.css';
 
 
 const Portfolio = () => {
     return (  
-      <div></div>
+        <div className="backgroundWrapper">
+  <div className="background">
+    <div className="portfolioContainer">
+      <div className="projectsContainer">
+        <div className="textContainer">
+          <div className="portfolioTitleContainer">
+            <h1 className="portfolioTitle">Portfolio</h1>
+          </div>
+        </div>
+        <div className="portfolioImageContainer">
+          {/* This can be left empty or removed if no longer needed */}
+        </div>
+      </div>
+      {/* Moved cake image here to position over the container */}
+      <div className="cakeContainer"></div>
+    </div>
+  </div>
+</div>
+
     );
   }
 
